f04e538fda0b794a82ac0d3e2f1074abeb7667d8
Eric19La <eric.la7722@gmail.com>
2025-10-07 17:44:22 -0700

    feat: change text and bg-color

--- frontend/my-app/components/Navbar.tsx
+++ frontend/my-app/components/Navbar.tsx
@@ -4,11 +4,11 @@ import { UserCircle } from 'lucide-react';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white shadow-lg">
+    <nav className="bg-gray-400 text-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-24">
           <div className="flex items-center">
-            <Link href="/" className="hover:opacity-60">
+            <Link href="/" className="hover:opacity-50">
               <Image
                 src="/ASI-Website-Logo_Brand-web.webp"
                 alt="ASICPP Logo"
@@ -20,10 +20,10 @@ export default function Navbar() {
           </div>
 
           <div className="flex space-x-4">
-            <Link href="/programs" className="hover:bg-gray-700 px-3 py-2 rounded-full flex items-center">
+            <Link href="/programs" className="text-sky-900 font-semibold hover:bg-gray-500 hover:text-sky-200 px-3 py-2 rounded-full flex items-center">
               Programs
             </Link>
-            <Link href="/profile" className="hover:bg-gray-700 px-3 py-2 rounded-full flex items-center gap-2">
+            <Link href="/profile" className="hover:bg-gray-500 px-3 py-2 rounded-full flex items-center gap-2">
               <UserCircle size={30} />
             </Link>
           </div>
